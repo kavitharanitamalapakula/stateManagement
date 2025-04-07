98241d10b930e0e2636ccd257374786cb37d29af
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-04-07 22:39:34 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import Counter from "./component/counter";
 import "./App.css"
-import Toggle from "./component/toggle";
-import Dropdown from "./component/Dropdown";
-import ControlCounter from "./component/ControlCounter";
+import Toggle from "./components/Toggle";
+import Dropdown from "./components/Dropdown";
+import ControlCounter from "./components/ControlCounter";
+import Counter from "./components/Counter";
+
 function App() {
   return (
     <>
-      <div style={{textAlign: "center"}}>
+      <div style={{ textAlign: "center" }}>
         <h3>Task-1</h3>
         <Counter />
       </div>
-      <div style={{textAlign: "center"}}>
+      <div style={{ textAlign: "center" }}>
         <h3>Task-2</h3>
         <Toggle />
       </div>
-      <div style={{textAlign: "center"}}>
+      <div style={{ textAlign: "center" }}>
         <h3>Task-3</h3>
         <Dropdown />
       </div>
-      <div style={{textAlign: "center"}}>
+      <div style={{ textAlign: "center" }}>
         <h3>Task-4</h3>
         <ControlCounter />
       </div>
